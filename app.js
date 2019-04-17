981aa8c8a843eec456087de711b63f397de2ec16
Matt Berry <matt@codeberry.ca>
2019-04-16 22:23:43 -0400

    finishing touches

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req,res) => {
-	let importantApplicant = [req.body.email, req.body.interests];
+	let importantApplicant = [req.body.email, req.body.interests]; // I've been working with objects
+	// recently, so ideally I'd work with those over arrays. I just had trouble console logging that.
 	res.render("thanks.ejs", {formInput: importantApplicant, email: req.body.email});
 });
 
